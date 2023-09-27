bf5826b32c2e085ebbea81082e871535301a0e7b
Alexandregv1080p <alexandregv1999@gmail.com>
2023-09-27 10:50:30 -0300

    commit

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -13,22 +13,31 @@ export default class Todo extends Component{
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
-        
+        this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.refresh()
     }
-    refresh(){
-        axios.get(`${URL}?sort=-createdAt`)
-            .then(resp => this.setState({...this.state,description:'',list:resp.data}))
+    refresh(description=''){
+        const search = description ? `&description__regex=/${description}/` : '' 
+        axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => this.setState({...this.state,description,list:resp.data}))
     }
     handleChange(e){
         this.setState({...this.state, description: e.target.value})
+    }
+    handleSearch(){
+        console.log('Funciona')
+        this.refresh(this.state.description)
     }
     handleAdd(){
         let description = this.state.description
         axios.post(URL, { description })
             .then(resp => this.refresh())
+    }
+    handleClear(){
+        this.refresh()
     }
     handleRemove(todo){
         axios.delete(`${URL}/${todo._id}`)
@@ -49,7 +58,10 @@ export default class Todo extends Component{
                 <TodoForm
                 handleAdd={this.handleAdd}
                 handleChange={this.handleChange}
-                description={this.state.description}/>
+                description={this.state.description}
+                handleSearch={this.handleSearch}
+                handleClear={this.handleClear}
+                />
                 <TodoList list={this.state.list}
                 handleMarkAsDone={this.handleMarkAsDone}
                 handleMarkAsPending={this.handleMarkAsPending}
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -8,11 +8,11 @@ export default props => {
             <tr key={todo._id}>
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style='succes' icon='check'
+                    <IconButton style='success' icon='check' hide={todo.done}
                     onClick={() => props.handleMarkAsDone(todo)}/>
-                    <IconButton style='warning' icon='undo'
+                    <IconButton style='warning' icon='undo' hide={!todo.done}
                     onClick={() => props.handleMarkAsPending(todo)}/>
-                    <IconButton style='danger' icon='trash-o'
+                    <IconButton style='danger' icon='trash-o' hide={!todo.done}
                     onClick={() => props.handleRemove(todo)}/>
                 </td>
             </tr>
@@ -25,7 +25,7 @@ export default props => {
                 <th>
                     Descrição
                 </th>
-                <th>
+                <th className="tableActions">
                     Ações
                 </th>
             </tr>
